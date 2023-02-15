e1c1e8f6967002242403ee1d6ce5b61cc253dc95
hannahpav <hannah.pavlovich@gmail.com>
2023-02-15 10:53:23 +0100

    Switching up colors

---
+++ plot3.R
@@ -0,0 +1,36 @@
+library(dplyr)
+library(lubridate)
+
+#Download the file
+#First check if it exists
+filename <- "power consumption.zip"
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(fileURL, filename, method = "curl")
+}
+
+#unzip the folder and download, if it doesn't exist
+if(!file.exists("power consumption")){
+  unzip(filename)
+}
+
+#read in the data and set the date to a real date
+table <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+table$Date <- as.Date(table$Date, "%d/%m/%Y")
+
+#Extract only two dates
+table2 <- subset(table, Date == "2007-02-01" | Date == "2007-02-02")
+table2$date_time <- with(table2, ymd(Date) + hms(Time)) # create a date + time column
+
+png(file= "plot3.png") # Open png file
+
+#Create the empty plot
+with(table2, plot(date_time, as.double(Sub_metering_1), type = "n", xlab = "", ylab = "Energy sub metering"))
+#Create the legend
+legend("topright", lwd = 1, col = c("black", "blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+#add the lines
+with(table2, lines(date_time, as.double(Sub_metering_1)))
+with(table2, lines(date_time, as.double(Sub_metering_2), col = "red"))
+with(table2, lines(date_time, as.double(Sub_metering_3), col = "blue"))
+
+dev.off() # Close PNG file
